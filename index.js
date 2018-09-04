e7008a3867cc9c1d33a2cf393fe2964f63d4caf5
Ryan Travis <rpenchenski@gmail.com>
2018-09-04 19:16:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function happyHolidays() {
-  return "Happy holidays!"
+  return "Happy holidays!";
 }
 
 function happyHolidaysTo(name) {
-  return 
+  return `Happy holidays, ${name}!`;
+}
+
+function happyHolidayTo(day, name) {
+  return `Happy ${day}, ${name}!`;
 }
 
 function holidayCountdown(holiday, days) {
-  
+  return `It's ${days} days until ${holiday}`;
 }
